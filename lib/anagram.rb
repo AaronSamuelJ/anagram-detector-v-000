db163c2b9d30ad00c6444a1fd58ff34990d5cb74
Aaron Samuel <aasamuelj@gmail.com>
2017-09-30 23:19:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
   def match(words)
     found = nil
     words.each do |word|
+      binding.pry
       word.split("").sort == @name.split("").sort ? found = word : []
     end
-    found
   end
 end
